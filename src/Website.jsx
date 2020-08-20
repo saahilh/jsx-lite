5009c983ae4524b8398bf738d2d3e70ff127ff98
Saahil Hamayun <saahilhamayun@hotmail.com>
2020-08-20 17:12:42 +0400

    Updated example to increase complexity

---
+++ src/Website.jsx
@@ -0,0 +1,24 @@
+import JsxLite from './jsx-lite';
+
+const Website = () => {
+    const getHeaderStyle = () => ({
+        textAlign: 'center',
+    });
+
+    const getMainStyle = () => ({
+        textAlign: 'center',
+    });
+
+    return (
+        <>
+            <header style={getHeaderStyle()}>
+                <h1>Hi, I'm Saahil</h1>
+            </header>
+            <main style={getMainStyle()}>
+                Who are you?
+            </main>
+        </>
+    );
+};
+
+export default Website;
--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,4 @@
 import JsxLite from './jsx-lite';
+import Website from './Website';
 
-var Hello = ({children}) => (<>{children}</>);
-
-var app = (
-  <Hello>
-    <div>Hi</div>
-    <span>Who are you?</span>
-  </Hello>
-);
-
-document.getElementById("root").innerHTML = app;
+document.getElementById("root").innerHTML = <Website />;
